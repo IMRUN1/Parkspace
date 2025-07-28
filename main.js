4e0fd01da7c7a208bad975f119de810d0281ce87
IMRUN1 <imrunext1@gmail.com>
2025-07-28 21:44:54 +0600

    Making cards

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 console.log("HELLO WORLD")
+
+let card = document.querySelectorAll(".cards")[0]
+let head = document.getElementById("Heading")
+let pgh = document.getElementById("Para")
+
+function createCard (){
+    console.log("created --1")
+    const str = document.createElement("div")
+    str.classList.add("ncrds")
+    card.appendChild(str)
+    const hdr = document.createElement("h2")
+    hdr.textContent = head.value
+    head.value = ""
+    str.appendChild(hdr)
+    const para = document.createElement("p")
+    para.textContent = pgh.value
+    pgh.value = ""
+    str.appendChild(para)
+}
